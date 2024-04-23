6d01bab53c4e61b84a69c818794d719035a66753
abhihek kumar <abhbiv5976@gmail.com>
2024-04-24 00:27:58 +0530

    api_view Decorator | Request Response | Django Rest Framwork

--- startapp/views.py
+++ startapp/views.py
@@ -1,52 +1,51 @@
 from django.shortcuts import render
-from django.http import JsonResponse,HttpResponse
 from .models import Employess
 from .serializer import EmployessSerializer, UsersSerializer
 from django.contrib.auth.models import User
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
 from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http import JsonResponse
 
-@csrf_exempt
+
+@api_view(['GET','POST'])
 def employessListView(request):
     if request.method  == 'GET':
         employess= Employess.objects.all()
-        serializer = EmployessSerializer(employess,many=True)
-        return JsonResponse(serializer.data , safe=False)
+        serializer = EmployessSerializer(employess, many=True)
+        return Response(serializer.data)
+    
     elif request.method == 'POST':
-        jsonData= JSONParser().parse(request)
-        serializer=EmployessSerializer(data=jsonData)
+        serializer=EmployessSerializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data , safe=False)
+            return Response(serializer.data)
         else:
-            return JsonResponse(serializer.errors , safe=False)
+            return Response(serializer.errors)
         
-        
-@csrf_exempt       
+@api_view(['GET','DELETE','PUT'])
 def EmployessDetailsview(request,pk):
 
     try:
         employess=Employess.objects.get(pk=pk)
     except Employess.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=404)
     
     if request.method == 'DELETE':
         employess.delete()
-        return HttpResponse(status = status.HTTP_204_NO_CONTENT)
+        return Response(status = status.HTTP_204_NO_CONTENT)
     
     elif request.method == 'GET':
         Serializer=EmployessSerializer(employess)
-        return JsonResponse(Serializer.data, safe=False)
+        return Response(Serializer.data)
     
     elif request.method == 'PUT':
-        jsonData= JSONParser().parse(request)
-        serializer=EmployessSerializer(employess , data=jsonData)
+        serializer=EmployessSerializer(employess , data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data , safe=False)
+            return Response(serializer.data)
         else:
-            return JsonResponse(serializer.errors , safe=False)
+            return Response(serializer.errors)
         
     
     
@@ -54,5 +53,5 @@ def EmployessDetailsview(request,pk):
 def UserListView(request):
     Users = User.objects.all() 
     serializer = UsersSerializer(Users , many=True)
-    return JsonResponse(serializer.data , safe=False)
+    return JsonResponse(serializer.data )
 
